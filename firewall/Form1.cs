14c8d1585c0a3b338d951205dd802a054b252f18
連 健傑 <z98765432156@gmail.com>
2020-03-07 18:42:48 +0800

    feat： 1、MD5掃描、黑名單判斷 2、新增MD5計算函數 3、新增Trace、Debug紀錄功能

--- firewall/Form1.cs
+++ firewall/Form1.cs
@@ -2,8 +2,11 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -21,10 +24,32 @@ namespace firewall
         {
             String startMenu = Environment.GetFolderPath(Environment.SpecialFolder.Startup);
 
+            List<String> blackList = new List<string>();
+            blackList.Add("7f1698bab066b764a314a589d338daae");
 
             foreach (String tempPath in System.IO.Directory.GetFiles(startMenu))
             {
-                MessageBox.Show(tempPath);
+                String tempMd5;
+                tempMd5 = CalculateMD5(tempPath);
+                if (blackList.Contains(tempMd5))
+                {
+                    MessageBox.Show("偵測到惡意檔案");
+                    Trace.WriteLine("偵測到惡意檔案" + "    " + tempPath, "MD5-掃描");
+                }
+
+                Trace.WriteLine(tempMd5 + "    " + tempPath, "MD5-紀錄");
+            }
+        }
+
+        static string CalculateMD5(string filename)
+        {
+            using (var md5 = MD5.Create())
+            {
+                using (var stream = File.OpenRead(filename))
+                {
+                    var hash = md5.ComputeHash(stream);
+                    return BitConverter.ToString(hash).Replace("-", "").ToLowerInvariant();
+                }
             }
         }
     }
